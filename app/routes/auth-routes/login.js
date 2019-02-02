767bb9a7700556bd57deec0674b5298d5a37be6f
Travis Grubbs <flygrubbs@yahoo.com>
2019-02-01 19:10:16 -0500

    login work

---
+++ app/routes/auth-routes/login.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+
+
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+
+router.post('/login', (req, res, next) => {
+    res.render('login');
+});
+
+module.exports = router;
