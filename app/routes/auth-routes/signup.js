1d16137518b7dbe1ddf340cda3dcd519f88fc83c
Travis Grubbs <flygrubbs@yahoo.com>
2019-02-04 23:54:46 -0500

    more signup work

--- app/routes/auth-routes/signup.js
+++ app/routes/auth-routes/signup.js
@@ -2,7 +2,57 @@ const express = require('express');
 const router  = express.Router();
 
 router.post('/signup', (req, res, next) => {
-    res.render('signup');
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+
+  // if(password.length < 7){
+  //     res.status(400).json({ message: 'Please make your password at least 8 characters long for security purposes.' });
+  //     return;
+  // }
+
+  User.findOne({ username }, (err, foundUser) => {
+
+      if(err){
+          res.status(500).json({message: "Username check went bad."});
+          return;
+      }
+
+      if (foundUser) {
+          res.status(400).json({ message: 'Username taken. Choose another one.' });
+          return;
+      }
+
+      const salt     = bcrypt.genSaltSync(10);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const aNewUser = new User({
+          username:username,
+          password: hashPass
+      });
+
+      aNewUser.save(err => {
+          if (err) {
+              res.status(400).json({ message: 'Saving user to database went wrong.' });
+              return;
+          }
+          
+          // Automatically log in user after sign up
+          // .login() here is actually predefined passport method
+          req.login(aNewUser, (err) => {
+
+              if (err) {
+                  res.status(500).json({ message: 'Login after signup went bad.' });
+                  return;
+              }
+          
+              // Send the user's information to the frontend
+              // We can use also: res.status(200).json(req.user);
+              res.json(aNewUser);
+          });
+      });
+  });
 });
 
 module.exports = router;
